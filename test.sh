e307f59950277d8a4a7574fe5187dedc17d054b9
UnnaturalTwilight <107954129+UnnaturalTwilight@users.noreply.github.com>
2025-12-07 01:25:53 -0500

    remove warning from test script

--- test.sh
+++ test.sh
@@ -11,14 +11,6 @@ if [[ "$1" =~ ^(debug|-debug|--debug|-d)$ ]]; then
     QT_IM_MODULE=qtvirtualkeyboard QML2_IMPORT_PATH=./components/ sddm-greeter-qt6 --test-mode --theme .
 else
     config_file=$(awk -F '=' '/^ConfigFile=/ {print $2}' metadata.desktop)
-    echo -e "${green}Testing Silent theme...${reset}\nLoading config: ${config_file}\nDon't worry about the infinite loading, SDDM won't let you log in while in 'test-mode'."
+    echo -e "${green}Testing Unnatural theme...${reset}\nLoading config: ${config_file}\nDon't worry about the infinite loading, SDDM won't let you log in while in 'test-mode'."
     QT_IM_MODULE=qtvirtualkeyboard QML2_IMPORT_PATH=./components/ sddm-greeter-qt6 --test-mode --theme . > /dev/null 2>&1
-fi
-
-if [ ! -d "/usr/share/sddm/themes/silent" ]; then
-    echo -e "\n${bred}[WARNING]: ${red}theme not installed!${reset}"
-    echo -e "Run ${cyan}'./install.sh'${reset} or copy the contents of the theme to ${cyan}'/usr/share/sddm/themes/silent/'${reset} and set the current theme to ${cyan}'silent'${reset} in ${cyan}'/etc/sddm.conf'${reset}:\n"
-    echo -e "    ${grey}# /etc/sddm.conf${reset}"
-    echo -e "    [Theme]"
-    echo -e "    Current=silent"
 fi
